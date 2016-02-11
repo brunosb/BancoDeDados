5247ea9b9ceefd1c7ec2957cf693220c5bd603f7
Bruno Silva Barbosa <brunosb09celular@gmail.com>
2016-02-11 13:55:36 -0300

    Finalizando botao de "Criar Tabelas"

--- src/bancodedados/Controllers/PrincipalController.java
+++ src/bancodedados/Controllers/PrincipalController.java
@@ -7,17 +7,23 @@ package bancodedados.Controllers;
 
 import bancodedados.BDConnect;
 import java.net.URL;
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.ToolBar;
 import javafx.scene.layout.AnchorPane;
@@ -51,14 +57,18 @@ public class PrincipalController implements Initializable {
     private Button btnFazerConsulta;
     private Button btnGerarProvas;
     public static TextArea sqlLog;
+    public static Connection bdConnect;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
         sqlLog = mTxtSqlLog;
+        sqlLog.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
+            sqlLog.setScrollTop(Double.MAX_VALUE);
+        });
         
         try {
-            conectarAoBanco();
+            bdConnect = conectarAoBanco();
         } catch (SQLException ex) {
             Logger.getLogger(PrincipalController.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -80,6 +90,47 @@ public class PrincipalController implements Initializable {
     
     private final EventHandler<ActionEvent> criandoTB = (ActionEvent event) -> {
         //codigo sql criando bd no jdbc
+        String[] sqlArray = new String[6];
+        
+        sqlArray[0] = "Create Table Professor "
+                + "(id serial primary key, "
+                + "nome varchar(50))";
+        sqlArray[1] = "Create Table Aluno "
+                + "(id serial primary key, "
+                + "nome varchar(50), "
+                + "professor_id int)";
+        sqlArray[2] = "Create Table Assunto "
+                + "(id serial primary key, "
+                + "disciplina varchar(30), "
+                + "professor_id int)";
+        sqlArray[3] = "Create Table Questao "
+                + "(id serial primary key, "
+                + "assunto_id int, "
+                + "pergunta varchar(255), "
+                + "resposta_id int)";
+        sqlArray[4] = "Create Table Resposta "
+                + "(id serial primary key, "
+                + "resposta varchar(255), "
+                + "questao_id int)";
+        sqlArray[5] = "Create Table Nota "
+                + "(id serial primary key, "
+                + "nota double precision, "
+                + "assunto_id int, "
+                + "aluno_id int)";
+        
+        try{
+            Statement s = bdConnect.createStatement();
+            deletarTabelas(s);
+            for(int i=0;i<sqlArray.length;i++){
+                s.execute(sqlArray[i]);
+                sqlLog.appendText("\n"+sqlArray[i]);
+            }
+            s.close();
+            
+        }catch(Exception e){
+            Alert a = new Alert(Alert.AlertType.ERROR, e.toString(), ButtonType.OK);
+            a.show();
+        }
     };
     
     private final EventHandler<ActionEvent> popularBD = (ActionEvent event) -> {
@@ -112,8 +163,18 @@ public class PrincipalController implements Initializable {
         }
     };
 
-    private void conectarAoBanco() throws SQLException {
+    private Connection conectarAoBanco() throws SQLException {
         BDConnect bd = new BDConnect();
-        bd.conect();
+        return bd.conect();
+    }
+
+    private void deletarTabelas(Statement s) throws SQLException {
+        s.execute("Drop table Professor");
+        s.execute("Drop table Aluno");
+        s.execute("Drop table Questao");
+        s.execute("Drop table Resposta");
+        s.execute("Drop table Assunto");
+        s.execute("Drop table Nota");
+        sqlLog.setText("Banco Resetado!!!");
     }
 }
